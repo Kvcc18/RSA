d40d4f74949129e8d141c9992577c2e03a0acd55
kvcc18 <karlacicler@hotmail.com>
2024-02-14 19:38:30 -0600

    RSA

--- rsa.py
+++ rsa.py
@@ -7,13 +7,36 @@
 #Imports
 import Crypto.Util.number
 
+
 #Número de bits
 bits = 1024
 
-#Obtener los primos para Alice y Bob
 
+#Obtener los primos para Alice y Bob
 pA = Crypto.Util.number.getPrime(bits, randfunc =  Crypto.Random.get_random_bytes)
 print("Primo de Alice", pA, "\n")
 
 qA = Crypto.Util.number.getPrime(bits, randfunc =  Crypto.Random.get_random_bytes)
 print("Primo de Alice", qA, "\n")
+
+pB = Crypto.Util.number.getPrime(bits, randfunc =  Crypto.Random.get_random_bytes)
+print("Primo de Bob", pB, "\n")
+
+qB = Crypto.Util.number.getPrime(bits, randfunc =  Crypto.Random.get_random_bytes)
+print("Primo de Bob", qB, "\n")
+
+
+#Obtener la primera parte de la llave pública de Alice y Bob
+nA = pA * qA
+print("nA: ", nA, "\n")
+
+nB = pB * qB
+print("nB: ", nB, "\n")
+
+
+#Calculamos el Indicador de Euler Phi
+phiA = (pA - 1) * (qA - 1)
+print("phiA: ", phiA, "\n")
+
+phiB = (pB - 1) * (qB - 1)
+print("phiB: ", phiB, "\n")
